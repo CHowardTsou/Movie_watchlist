9b320215568c5c8aee9f93bf96e4c490f825177c
CHowardTsou <127212524+CHowardTsou@users.noreply.github.com>
2024-02-15 22:47:08 +0800

    fix delete issue
    modify delete method

--- watchlist.js
+++ watchlist.js
@@ -1,11 +1,14 @@
-const watchlist = JSON.parse(localStorage.getItem("watchlist")) || []
+let watchlist = JSON.parse(localStorage.getItem("watchlist")) || []
 const displayResult = document.getElementById('display-result')
 
 function render(searchArr){
     if(searchArr.length === 0){
         displayResult.innerHTML = `<div class="emptylist">
                                     <span class="empty">Your watchlist is looking a little empty...</span>
-                                    <p class="empty-two"><span class="material-symbols-outlined">add_circle</span>Let's add some movies!</p>
+                                    <div class="empty-text">
+                                        <i class="fa-solid fa-circle-plus"></i>
+                                        <p class="empty-two">Let's add some movies!</p>
+                                    </div>
                                    </div> 
                                     `
     }
@@ -23,10 +26,8 @@ function render(searchArr){
                                 <p>${movie.Runtime}</p>
                                 <p>${movie.Genre}</p>
                                 <div class="watchlist">
-                                    <span class="material-symbols-outlined" 
-                                        data-remove=${movie.imdbID}>
-                                        cancel</span>
-                                    <p>Remove</p>
+                                    <p>
+                                    <i class="fa-solid fa-circle-minus" data-remove=${movie.imdbID}></i>Remove</p>
                                 </div>
                             </div>
                             <p class="plot">
@@ -42,46 +43,17 @@ function render(searchArr){
         displayResult.innerHTML = html
         
     }
-    
-    // for(let search of searchArr){
-    //     displayResult.innerHTML += `
-    //             <div class="result">
-    //                 <img src=${search.Poster}>
-    //                 <div class="result-right">
-    //                     <div class="name-score">
-    //                         <h3>${search.Title}</h3>
-    //                         <p>⭐${search.imdbRating}</p>
-    //                     </div>
-    //                     <div class="min-type">
-    //                         <p>${search.Runtime}</p>
-    //                         <p>${search.Genre}</p>
-    //                         <div class="watchlist">
-    //                             <span 
-    //                                 class="material-symbols-outlined" 
-    //                                 data-imdbid=${search.imdbID}>
-    //                                 add_circle
-    //                             </span>
-    //                             <p>Remove</p>
-    //                         </div>
-    //                     </div>
-    //                     <p class="plot">
-    //                         ${search.Plot}
-    //                     </p>
-    //                 </div>
-                    
-    //             </div>
-    //             <hr />
-    //             `
-    // }
-    
 }
 
 render(watchlist)
 
 document.addEventListener("click", function(e){
     if(e.target.dataset.remove){
-        let newArray = watchlist.filter(movie => movie.imdbID != e.target.dataset.remove)
-        localStorage.setItem("watchlist", JSON.stringify(newArray))
-        render(newArray)
+        newArray = watchlist.filter(movie => movie.imdbID != e.target.dataset.remove)
+        watchlist.length = 0;
+        watchlist = newArray 
+        localStorage.setItem("watchlist", JSON.stringify(watchlist))
+        
+        render(watchlist)
     }
 })
